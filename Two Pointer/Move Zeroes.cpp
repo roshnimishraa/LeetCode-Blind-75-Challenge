f2f1fedf047159e955e0ed04519523a78a9351ee
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-05 00:14:24 +0530

    Update Move Zeroes.cpp

--- Two Pointer/Move Zeroes.cpp
+++ Two Pointer/Move Zeroes.cpp
@@ -1,1 +1,21 @@
+TC : O(N) 
+SC : O(1) 
 
+Algo 
+1. Intialized a pivot as 0 ;
+2. if nums[i] != 0 then swap pivot++ 
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pivot = 0;
+int n=nums.size();
+for(int i=0;i<n;i++){
+    if(nums[i] != 0){
+        swap(nums[i],nums[pivot]);
+        pivot++;
+    }
+}
+    }
+};
