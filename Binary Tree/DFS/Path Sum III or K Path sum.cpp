ba6e3325bb48bcc14b94f8ed6c492ef1cfc769b4
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-07 13:55:33 +0530

    Update Path Sum III or K Path sum.cpp

--- Binary Tree/DFS/Path Sum III or K Path sum.cpp
+++ Binary Tree/DFS/Path Sum III or K Path sum.cpp
@@ -1,3 +1,21 @@
+Time Complexity: O(N), where N is the number of nodes in the tree
+Space Complexity: O(N), where N is the number of nodes in the tree 
+
+Approach : DFS + RECURSION + PREORDER TRAVERSAL
+
+ALGORITHM
+    take sum=0,count=0 and vector<int> path
+1. First store curr root val in vector 
+2. traverse left and right subtree 
+3. iterate nodes till vector size 
+   add nodes sum+= path[i]
+i) if sum == targetSum then increment count do 
+    count++
+4. while backtracking remove previous nodes 
+ path.pop_back()
+5. return count
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -10,32 +28,38 @@
  * };
  */
 class Solution {
-void helper(TreeNode* root,int k,int &count,vector<int> path)
-{
-if(root == NULL) return;
-// curr node 
+    void helper(TreeNode* root,int targetSum,
+    vector<int> path,int &count)
+    {
+        if(root == NULL) return;
+// push curr node value into vector
 path.push_back(root->val);
 // left subtree 
-helper(root->left,k,count,path);
-// right subtree
-helper(root->right,k,count,path);
-
-int n = path.size();
+helper(root->left,targetSum,path,count);
+// right subtree 
+helper(root->right,targetSum,path,count);
+// insert node dfs traversal 
+int n=path.size();
 long long sum=0;
+
 for(int i=n-1;i>=0;i--)
 {
+// add sum of nodes
 sum += path[i];
-if(sum == k){
+// if targetSum found
+if(sum == targetSum){
     count++;
+
 }
 }
+// remove node
 path.pop_back();
-}
+    }
 public:
     int pathSum(TreeNode* root, int targetSum) {
         vector<int> path;
         int count=0;
-    helper(root,targetSum,count,path);
+        helper(root,targetSum,path,count);
     return count;
     }
 };
