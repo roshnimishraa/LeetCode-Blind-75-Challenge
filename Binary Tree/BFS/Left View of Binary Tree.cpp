6bfe07d3aa628de9f9e07f60ed5fff87c3a7c135
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-07 18:41:13 +0530

    Update Left View of Binary Tree.cpp

--- Binary Tree/BFS/Left View of Binary Tree.cpp
+++ Binary Tree/BFS/Left View of Binary Tree.cpp
@@ -1,3 +1,17 @@
+
+Approach : RECURSION + vector to store right view 
+
+TC : O(N) 
+SC : O(H) 
+
+level=0
+Algorithm 
+1. to get right view LAST NODE OF EVERY LEVEL MUST BE INSERTED 
+So, if Level == ans.size then push element 
+2. recursion call for left and right subtree
+3. return ans
+
+
 void recursion(Node *root,int level,vector<int> &ans)
 {
     if(root==NULL) return;
