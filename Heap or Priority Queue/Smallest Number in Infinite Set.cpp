faca3efdcadc8cb3f7545f494423785d31fc0dfd
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-08 13:46:01 +0530

    Update Smallest Number in Infinite Set.cpp

--- Heap or Priority Queue/Smallest Number in Infinite Set.cpp
+++ Heap or Priority Queue/Smallest Number in Infinite Set.cpp
@@ -37,9 +37,86 @@ return result;
     }
 };
 
-/**
- * Your SmallestInfiniteSet object will be instantiated and called as such:
- * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
- * int param_1 = obj->popSmallest();
- * obj->addBack(num);
- */
+##########################################################################################################################################
+
+Better Approach : Min-Heap +  Unordered_set 
+
+Why Min-Heap & unorderd_set
+
+------> min-heap : to store the min element at top so that it is easy to get pop Smallest
+------> unordered_set : to search in O(1) time weather the element is exists or not 
+
+class SmallestInfiniteSet {
+public:
+int currSmallest;
+priority_queue<int,vector<int>,greater<int>> pq;
+unordered_set<int> st;
+    SmallestInfiniteSet() {
+        currSmallest = 1;
+    }
+    
+    int popSmallest() {
+        int result;
+// if element exist
+if(!pq.empty())
+{
+    result = pq.top();
+    pq.pop();
+    st.erase(result);
+}
+else{
+    result = currSmallest;
+    currSmallest += 1;
+}
+        return result;
+    }
+    
+    void addBack(int num) {
+//  corner case 
+// if num > current Smallest or if it is already exist 
+if(num >= currSmallest || st.find(num) != st.end())
+{
+    return;
+}
+pq.push(num);
+st.insert(num);
+    }
+};
+
+######################### EFFICIENT APPROACH 
+Time complexity:O(n∗logn)
+Space complexity:O(n)
+
+class SmallestInfiniteSet {
+public:
+int currSmallest;
+set<int> st;
+    SmallestInfiniteSet() {
+        currSmallest = 1;
+    }
+       int popSmallest() {
+        int result;
+// if element exist
+if(!st.empty())
+{
+// *(st.begin()) -> to get top element from set
+    result = *(st.begin());
+    st.erase(result);
+}
+else{
+    result = currSmallest;
+    currSmallest += 1;
+}
+        return result;
+} 
+    void addBack(int num) {
+//  corner case 
+// if num > current Smallest or if it is already exist 
+if(num >= currSmallest || st.find(num) != st.end())
+{
+    return;
+}
+st.insert(num);
+    }
+};
+
