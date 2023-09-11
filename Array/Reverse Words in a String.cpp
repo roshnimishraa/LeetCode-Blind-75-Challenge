bdecc5a1f170272a426c4da26ed862e6272bee19
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-11 21:44:25 +0530

    Create Reverse Words in a String.cpp

---
+++ Array/Reverse Words in a String.cpp
@@ -0,0 +1,53 @@
+TC : O(N) 
+SC : O(1) 
+  
+class Solution {
+public:
+    string reverseWords(string s) {
+            int n =s.length();
+        int left = 0 , right=0;
+    int i=0;
+        
+//  1. Reverse the entire array
+        reverse(s.begin(),s.end());
+        while(i < n){    
+    while(i < n && s[i] == ' '){
+        i++;
+    } 
+while(i< n && s[i] != ' '){
+    s[right++] = s[i++];   
+}
+//2. Reverse word 
+        if(left < right){
+            reverse(s.begin()+left,s.begin()+right);       
+             // Add a space after the reversed word
+        s[right++]=' ';
+   
+//update the left index for the next word
+            left = right;
+        }
+    i++;
+        }
+         // Add a space after the reversed word
+      s = s.substr(0,right-1);
+        return s;
+    }
+};
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
+APPROACH 2 : USING STING STREAM FUNCTION 
+
+class Solution {
+public:
+    string reverseWords(string s) {
+     stringstream ss(s);
+     string token,result;
+     while(ss >> token)
+     {
+         result = token+" "+result;
+
+     }  
+     return result.substr(0,result.size()-1);
+    }
+};
